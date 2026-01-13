a164c675ed7dfdd69e548b4cc970aa51ac8abf04
swets98 <ladybirdsweta@gmail.com>
2026-01-13 22:37:55 +0530

    Added Packages(SeleniumExtras WaitHelper, Support, WebDriver, WebDriverManager) under dependencies & Added a new .cs file : Checking whether [Setup] running before each [Test]

---
+++ SeleniumTestAutomation/SeleniumTest/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace SeleniumTest
+{
+    public class Tests
+    {
+        /// <summary>
+        //SetUp will run before each and every tests(ie once for test1 and again for test 2)
+        ///Check output to understand
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            //Use "TestContext.Progress.WriteLine" : To log value of NUnit, instead of using "Console.WriteLine" 
+            TestContext.Progress.WriteLine("Setting up tests...");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            TestContext.Progress.WriteLine("This is test1");
+        }
+        [Test]
+        public void Test2()
+        {
+            TestContext.Progress.WriteLine("This is test2");
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            TestContext.Progress.WriteLine("Tearing down tests...");
+        }
+    }
+}
